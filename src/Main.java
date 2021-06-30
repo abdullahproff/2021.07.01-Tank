e8b096cf3e5c1ba8347961f74c4b54c444673413
AbdullahAbdullaev <amabdullaevproff@gmail.com>
2021-06-30 20:43:01 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int i1 = 5;
+        int i2 = 11;
+        double d1 = 5.5;
+        double d2 = 1.3;
+        long l = 20L;
+        double result = 0;
+        System.out.println(result = i2 / d1 + d2 % i1 - l);
+        int a = 5;
+        int resultA = a-- - --a + ++a + a++ + a;
+        int b = 8;
+        int resultB = ++b - b++ + ++b - --b;
+
+        System.out.println(resultA);
+        System.out.println(resultB);
+    }
+}
---
+++ src/Tank.java
@@ -0,0 +1,50 @@
+public class Tank {
+
+    int x, y;
+    int dir;
+    int fuel;
+
+    public Tank() {
+        this(0, 0, 100);
+    }
+
+    public Tank(int x, int y) {
+        this(x, y, 100);
+    }
+
+    public Tank(int x, int y, int fuel) {
+        this.x = x;
+        this.y = y;
+        this.fuel = fuel;
+    }
+
+    public void goForward(int i) {
+        if (i < 0 && -i > fuel)
+            i = -fuel;
+        else if (i > fuel)
+            i = fuel;
+        if (dir == 0) x += i;
+        else if (dir == 1) y += i;
+        else if (dir == 2) x -= i;
+        else y -= i;
+        fuel -= Math.abs(i);
+    }
+
+    public void printPosition() {
+        System.out.println("The Tank is at " + x +", " + y + " now.");
+    }
+
+    public void turnLeft() {
+        dir--;
+        if (dir == -1) dir = 3;
+    }
+
+    public void turnRight() {
+        dir++;
+        if (dir == 4) dir = 0;
+    }
+
+    public void goBackward(int i) {
+        goForward(-i);
+    }
+}
